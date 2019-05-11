9210a6ce7d866d5b453ffc894f659cee9ba444d2
Phuong Phuc <phuong.hophuc@mor.com.vn>
2019-05-11 15:08:29 +0700

    Add UBot construction with initial state and func to convert UBot angle to direction

---
+++ core.py
@@ -0,0 +1,37 @@
+import math
+from enum import Enum
+
+
+class Action(Enum):
+    """Actions that UBot can understand"""
+    TURN_LEFT = 'L'
+    TURN_RIGHT = 'R'
+    WALK = 'W'
+
+
+class UBot:
+    def __init__(self, x=0, y=0, angle=90):
+        """
+        Initial state of UBot
+        default: x = 0, y = 0, UBot is facing to the North, equivalent with 90 degree
+        """
+        self.x = x
+        self.y = y
+        self.angle = angle
+
+    @staticmethod
+    def convert_angle_to_direction(angle):
+        """convert angle to direction"""
+        _rad = math.radians(angle)
+        _sin = math.sin(_rad)
+        _cos = math.cos(_rad)
+        if -1 < _sin < 1:
+            if _cos == 1:
+                return 'East'
+            elif _cos == -1:
+                return 'West'
+        elif _sin == 1:
+            return 'North'
+        elif _sin == -1:
+            return 'South'
+        return 'Unidentified (angle: {})'.format(angle)
